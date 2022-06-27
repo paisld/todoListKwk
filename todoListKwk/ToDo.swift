2c9b53768e7fef363cbb08bd003b91dea1b96dd0
Paisley <paisley.lucier@gmail.com>
2022-06-27 16:49:22 -0500

    working

---
+++ todoListKwk/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  todoListKwk
+//
+//  Created by Scholar on 6/27/22.
+//
+import UIKit
+
+class ToDo {
+    var name = "You should never see this."
+    var important = false
+    
+}
